d2ccf1cd4a80f8a9b9c438da7ec6fb5a74cc0841
Tetyana Yarish <avamkuda@gmail.com>
2021-10-12 22:55:39 +1100

    Rectangle

--- Rectangle/Program.cs
+++ Rectangle/Program.cs
@@ -13,37 +13,33 @@ namespace Rectangle
             string space = " ";
             int l = 1;
             Program pr = new();
-           
+
             Console.WriteLine("Insert length of rectangle.");
-            int width=Convert.ToInt32(Console.ReadLine());
-            
+            int width = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Insert width of rectangle.");
             int length = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i < width; i++)
+            Console.Write(space);
+            for (int i = 0; i < width; i++)
             {
-               // space = oneRow;
-            //Console.Write(oneRow);
-            //} 
-            //for (int p = 0; p < length; p++)
-            //{
-            Console.WriteLine(pr.Column());
-               
+
+                Console.Write(oneRow);
             }
-            for (int i = 1; i < width; i++)
-                {
-                   
-                    Console.Write(oneRow);
-                }
+            for (int p = 0; p < length; p++)
+            {
+                Console.Write("\n"+pr.Column());
+
+            }
+            for (int i = 0; i < width; i++)
+            {
+
+                Console.Write(oneRow);
+            }
         }
         public string Column()
         {
 
             string oneColumn = "|";
-            for (int p =1; p < length; p++)
-            {
-                Console.WriteLine(oneColumn);
-
-            }
+            
             return oneColumn;
 
         }
